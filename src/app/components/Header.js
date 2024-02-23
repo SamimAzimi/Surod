174fa70da3999270d413d5d3af5a2a0d2b089814
Samim Azimi <samimelite8@gmail.com>
2024-02-24 00:09:53 +0200

    header modified

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import SurodLogo from '../assets/logo.png'
 import Image from 'next/image'
+import {db} from '../firebaseConfig'
 function Header(setUpdateSong) {
+  
+  const [search,setSearch] = useState()
+
   
   return (
     <div class='header'>
-      <input type="search" name="search" class='searchbar' placeholder="جستجو آهنگ"/>
+      <input type="search" name="search" value={search} onChange={setSearch} class='searchbar' placeholder="جستجو آهنگ"/>
       <div class='logoContainer'>
         <Image src={SurodLogo} alt="Surod App" />
       </div>
