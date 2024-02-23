1825511bb1c010c98d4e5e7f7256c3fdcc32636d
Subhan <52883561+SamimAzimi@users.noreply.github.com>
2024-02-24 01:16:40 +0200

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,14 +2,21 @@ import React,{useState} from 'react'
 import SurodLogo from '../assets/logo.png'
 import Image from 'next/image'
 import {db} from '../firebaseConfig'
+import {getDoc, collection} from'firebase/firestore'
 function Header(setUpdateSong) {
-  
+  const collectionRef = collection(db,'lyrics')
   const [search,setSearch] = useState()
-
+  const getDoc = ()=>{
+    if (search)
+    {
+      getDoc(collectionRef).then((data)=>{console.log(data)})
+    }
+  }
   
   return (
     <div class='header'>
       <input type="search" name="search" value={search} onChange={setSearch} class='searchbar' placeholder="جستجو آهنگ"/>
+      <button type="submit" class="searchBtn" onClick={getDoc}>جستجو</button>
       <div class='logoContainer'>
         <Image src={SurodLogo} alt="Surod App" />
       </div>
