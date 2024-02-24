90d26fd558a48e030bb8fd2a792693ab85a69a99
Subhan <52883561+SamimAzimi@users.noreply.github.com>
2024-02-24 02:02:53 +0000

    Sat, Feb 24, 2024, 4:02 AM +02:00

---
+++ src/app/Allsong.js
@@ -0,0 +1,15 @@
+import React, {useState }from 'react'
+import {db} from '../firebaseConfig'
+import { collection, addDoc} from 'firebase/firestore'
+import {toast} from 'react-toastify'
+function AllSong() {
+    const collectionref = collection(db, "lyrics")
+
+  return (
+    <>
+    <h1>All Songs</h1>
+    </>
+  )
+}
+
+export default AllSong
