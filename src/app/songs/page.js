b563eb5e04e27c80be5402c3a58b5ab1699a333c
Subhan <52883561+SamimAzimi@users.noreply.github.com>
2024-02-24 02:20:56 +0000

    Sat, Feb 24, 2024, 4:20 AM +02:00

--- src/app/songs/page.js
+++ src/app/songs/page.js
@@ -1,13 +1,21 @@
-import React, {useState }from 'react'
+import React, useEffect from 'react'
 import {db} from '../firebaseConfig'
 import { collection, addDoc} from 'firebase/firestore'
 import {toast} from 'react-toastify'
 function page() {
     const collectionref = collection(db, "lyrics")
+    const [songs,allSongs] = useState()
+
+    useEffect(()=>{
+        
+    },[])
 
   return (
     <>
-    <h1>All Songs</h1>
+    <h1>Back To Home</h1>
+    <ul>
+      <li><h1></h1></li>
+    </ul>
     </>
   )
 }
