615e892c10ae2800fe5f2e0c4b6a820224b05422
Subhan <52883561+SamimAzimi@users.noreply.github.com>
2024-02-24 01:53:30 +0000

    Sat, Feb 24, 2024, 3:53 AM +02:00

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,6 +3,8 @@ import SurodLogo from '../assets/logo.png'
 import Image from 'next/image'
 import {db} from '../firebaseConfig'
 import { collection, query, where,getDocs } from "firebase/firestore";
+import {toast} from 'react-toastify'
+
 function Header({setUpdateSong}) {
   const collectionRef = collection(db,'lyrics')
   const [search,setSearch] = useState()
@@ -11,6 +13,7 @@ function Header({setUpdateSong}) {
     {
       const q = query(collectionRef, where("title", "==", search));
       const querySnapshot = await getDocs(q);
+      if (!querySnapshot.doc) {toast.info("آهنگ شما دریافت نگردید");setSearch('')}
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data(), doc);
@@ -24,8 +27,10 @@ function Header({setUpdateSong}) {
 
   return (
     <div class='header'>
-      <input type="search" name="search" value={search} onChange={(e)=>setSearch(e.target.value)} class='searchbar' placeholder="جستجو آهنگ"/>
+      <div class="searchGrp">
       <button type="submit" class="searchBtn" onClick={getDoc}>جستجو</button>
+      <input type="search" name="search" value={search} onChange={(e)=>setSearch(e.target.value)} class='searchbar' placeholder="جستجو آهنگ"/>
+      </div>
       <div class='logoContainer'>
         <Image src={SurodLogo} alt="Surod App" />
       </div>
